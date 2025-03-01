4540648630e52fa93aebf4a67ec803137af21e27
Sawaki Shuto <swkshuto0208@icloud.com>
2025-03-01 18:10:03 +0900

    initial

---
+++ MusicNFC/MusicNFCApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicNFCApp.swift
+//  MusicNFC
+//
+//  Created by 澤木柊斗 on 2025/03/01.
+//
+
+import SwiftUI
+
+@main
+struct MusicNFCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MusicNFCTests/MusicNFCTests.swift
@@ -0,0 +1,17 @@
+//
+//  MusicNFCTests.swift
+//  MusicNFCTests
+//
+//  Created by 澤木柊斗 on 2025/03/01.
+//
+
+import Testing
+@testable import MusicNFC
+
+struct MusicNFCTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
